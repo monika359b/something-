43ff8c8249a7306875b8c5d1978111f4d4c26189
Amith-Coder <92964353+Amith-Coder@users.noreply.github.com>
2021-11-23 20:16:46 +0530

    Update and rename 💵 Balance 💵.js to _balance.js

--- commands/Main Menu/_balance.js
+++ commands/Main Menu/_balance.js
@@ -1,12 +1,12 @@
 /*CMD
-  command: 💵 Balance 💵
+  command: /balance
   help: 
   need_reply: 
   auto_retry_time: 
   folder: Main Menu
   answer: 
   keyboard: 
-  aliases: 
+  aliases: 💵 Balance 💵
 CMD*/
 
 let stat = Bot.getProperty("" + user.telegramid + "")
