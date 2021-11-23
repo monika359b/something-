4c7c90390e4dc15b71b375e0ccba37af9692f7e1
Amith-Coder <92964353+Amith-Coder@users.noreply.github.com>
2021-11-23 20:13:35 +0530

    Update 🤴Referral🤴.js

--- commands/Main Menu/🤴Referral🤴.js
+++ commands/Main Menu/🤴Referral🤴.js
@@ -1,12 +1,12 @@
 /*CMD
-  command: 🤴Referral🤴
+  command: /refferal
   help: 
   need_reply: 
   auto_retry_time: 
   folder: Main Menu
   answer: 
   keyboard: 
-  aliases: 
+  aliases: 🤴Referral🤴
 CMD*/
 
 let stat = Bot.getProperty("" + user.telegramid + "")
