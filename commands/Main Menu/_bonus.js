ac22014933eccb2486e20f475c1ced8d46b000e9
Amith-Coder <92964353+Amith-Coder@users.noreply.github.com>
2021-11-23 20:16:09 +0530

    Update and rename 🎁 Bonus 🎁.js to _bonus.js

--- commands/Main Menu/_bonus.js
+++ commands/Main Menu/_bonus.js
@@ -1,12 +1,12 @@
 /*CMD
-  command: 🎁 Bonus 🎁
+  command: /bonus
   help: 
   need_reply: 
   auto_retry_time: 
   folder: Main Menu
   answer: 
   keyboard: 
-  aliases: 
+  aliases: 🎁 Bonus 🎁
 CMD*/
 
 let stat = Bot.getProperty("" + user.telegramid + "")
