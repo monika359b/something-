ecab3801053582181c1b728b9a35df7b3a6d128c
Amith-Coder <92964353+Amith-Coder@users.noreply.github.com>
2021-11-24 13:10:37 +0530

    Update and rename ✅Joined.js to _joined.js

--- commands/Joining System/_joined.js
+++ commands/Joining System/_joined.js
@@ -1,12 +1,12 @@
 /*CMD
-  command: ✅Joined
+  command: /joined 
   help: 
   need_reply: 
   auto_retry_time: 
   folder: Joining System
   answer: 
   keyboard: 
-  aliases: 
+  aliases: ✅Joined
 CMD*/
 
 let stat = Bot.getProperty("" + user.telegramid + "")
